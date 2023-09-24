d66ed854d46344564d3026c025185cf897bb04a2
Poorva5 <poorvak455@gmail.com>
2023-09-24 17:29:40 +0530

    Add update api

--- invoice_app/serializers.py
+++ invoice_app/serializers.py
@@ -25,5 +25,27 @@ class InvoiceSerializer(serializers.ModelSerializer):
         return invoice
     
     
-    
+    def update(self, instance, validated_data):
+        invoice_details_data = validated_data.pop('invoice_details', [])
+
+        # Update invoice fields
+        instance.date = validated_data.get('date', instance.date)
+        instance.customer_name = validated_data.get('customer_name', instance.customer_name)
+        instance.save()
+
+        # Update or create invoice details
+        for detail_data in invoice_details_data:
+            detail_id = detail_data.get('id')
+            if detail_id:
+                # If an ID is provided, update the existing invoice detail
+                detail = InvoiceDetail.objects.get(id=detail_id, invoice=instance)
+                detail.description = detail_data.get('description', detail.description)
+                detail.quantity = detail_data.get('quantity', detail.quantity)
+                detail.unit_price = detail_data.get('unit_price', detail.unit_price)
+                detail.save()
+            else:
+                # If no ID is provided, create a new invoice detail
+                InvoiceDetail.objects.create(invoice=instance, **detail_data)
+
+        return instance
     
--- invoice_app/tests/test_views.py
+++ invoice_app/tests/test_views.py
@@ -1,3 +1,4 @@
+from invoice_server.invoice_app.models import InvoiceDetail
 from utils.test_utils import APITest
 from rest_framework import status
 from django.urls import reverse
@@ -39,3 +40,64 @@ class InvoiceTestCase(APITest):
         self.assertEqual(len(response.data), 3) 
     
   
+    
+    def test_update_invoice_with_invoice_details(self):
+        # Create an initial invoice with some details
+        initial_invoice = InvoiceFactory.create()
+        initial_details = [
+            {
+                "description": "Initial Product",
+                "quantity": 1,
+                "unit_price": 20.0,
+            }
+        ]
+        for detail_data in initial_details:
+            InvoiceDetail.objects.create(invoice=initial_invoice, **detail_data)
+
+        # Define the updated invoice data
+        updated_invoice_data = {
+            "date": "2023-09-26",
+            "customer_name": "Updated Customer",
+            "invoice_details": [
+                {
+                    "id": initial_invoice.invoice_details.first().id,  
+                    "description": "Updated Product",
+                    "quantity": 3,
+                    "unit_price": 15.0,
+                },
+                {
+                    
+                    "description": "New Product",
+                    "quantity": 2,
+                    "unit_price": 10.0,
+                },
+            ],
+        }
+
+        # Prepare the URL for updating the invoice
+        url = reverse('invoice-detail', kwargs={'pk': initial_invoice.pk})
+
+        # Send a PUT request to update the invoice
+        response = self.client.put(url, data=updated_invoice_data, format="json")
+
+        # Check the response status code
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Refresh the initial invoice instance from the database
+        initial_invoice.refresh_from_db()
+
+        # Check if the invoice and details are updated correctly
+        self.assertEqual(initial_invoice.date, "2023-09-26")
+        self.assertEqual(initial_invoice.customer_name, "Updated Customer")
+
+        # Check the updated detail
+        updated_detail = initial_invoice.invoice_details.first()
+        self.assertEqual(updated_detail.description, "Updated Product")
+        self.assertEqual(updated_detail.quantity, 3)
+        self.assertEqual(updated_detail.unit_price, 15.0)
+
+        # Check the newly created detail
+        new_detail = initial_invoice.invoice_details.last()
+        self.assertEqual(new_detail.description, "New Product")
+        self.assertEqual(new_detail.quantity, 2)
+        self.assertEqual(new_detail.unit_price, 10.0)
